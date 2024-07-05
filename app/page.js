1aeaaac8f794725bdcc8686986097c24972bfa15
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-07-05 22:07:26 +0300

    list

--- app/page.js
+++ app/page.js
@@ -1,16 +1,28 @@
-import './globals.css'
+import './globals.css';
 
-const menuList = ['Vehicles', 'Sell', 'Buy', 'About', 'FAQs'];
+const menuList = ['Sell a car', 'Buy a car'];
 
 export default function Page() {
     return (
       <div className='bg-gray-100 min-h-screen'>
-        <div className=''>
-          <h1 className='font-bold text-3xl p-4'>autoX.com</h1>
+        <div className='flex justify-between items-center p-4'>
+          <div>
+            <h1 className='font-bold text-3xl'>autoX.com</h1>
+          </div>
+          <div>
+            <ul className='flex space-x-0 divide-x divide-gray-500'>
+              <li className='bg-black text-white px-4 py-2 hover:cursor-pointer'>
+                {menuList[0]}
+              </li>
+              <li className='bg-white text-black px-4 py-2 hover:cursor-pointer'>
+                {menuList[1]}
+              </li>
+            </ul>
+          </div>
         </div>
         <div>
-          
+          {/* Other content */}
         </div>
       </div>
-  );
+    );
 }
