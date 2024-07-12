cc9493d7c7404133daa481b35e99b38ce5fcc4a8
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-07-12 11:26:42 +0300

    perks heading

--- app/components/perkslist.js
+++ app/components/perkslist.js
@@ -8,6 +8,9 @@ import { faToolbox } from '@fortawesome/free-solid-svg-icons';
 export default function Perkslist() {
   return (
     <div className="flex flex-col items-center mt-4 pt-4">
+      <div className='border-b-8 m-8 p-4'>
+        <h1 className='text-white font-bold text-4xl'>Our Perks</h1>
+      </div>
       <ul className="flex flex-wrap justify-center space-x-4 w-full">
         <li className="w-full max-w-xs">
           <div className="bg-gray-300 flex justify-center items-center h-48 rounded-lg shadow-lg p-4">
